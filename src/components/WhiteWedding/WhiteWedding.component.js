555a9491661ecbc4088cf17693f4279f8680e41c
Dominik Broj <dominik.broj@dataart.com>
2017-02-28 18:40:28 +0100

    initial version of WhiteWedding component

--- src/components/WhiteWedding/WhiteWedding.component.js
+++ src/components/WhiteWedding/WhiteWedding.component.js
@@ -4,10 +4,16 @@ import SectionHeading from '../SectionHeading/SectionHeading.component';
 import SectionText from '../SectionText/SectionText.component';
 
 const WhiteWedding = ({ imgUrl, text }) => {
+  const imgStyle = {
+    background: 'url(' + imgUrl + ') center no-repeat'
+  };
   return (
     <section className={styles.WhiteWedding}>
       <SectionHeading text="ślub" />
       <SectionText text={text} />
+      <div className={styles.imgWrapper}>
+        <div style={imgStyle} className={styles.img}/>
+      </div>
     </section>
   )
 };
