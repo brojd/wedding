074cb5b52746115f742eae2b8f674485c1af185b
Dominik Broj <dominik.broj@dataart.com>
2017-02-28 17:34:35 +0100

    create HeadingUnderline component

---
+++ src/components/HeadingUnderline/HeadingUnderline.component.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './HeadingUnderline.stylesheet.css';
+import classNames from 'classnames';
+
+const HeadingUnderline = ({ width }) => {
+  const lineStyle = {
+    width: width / 2
+  };
+  return (
+    <div>
+      <div style={lineStyle} className={styles.line}></div>
+      <span className={classNames("glyphicon glyphicon-grain", styles.icon)}></span>
+      <div style={lineStyle} className={styles.line}></div>
+    </div>
+  )
+};
+
+export default HeadingUnderline;
