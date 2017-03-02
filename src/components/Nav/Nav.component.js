6efb5c2493046f37dc9eb6ee612db5b16bc24925
Dominik Broj <dominik.broj@dataart.com>
2017-03-02 14:13:09 +0100

    style Nav

--- src/components/Nav/Nav.component.js
+++ src/components/Nav/Nav.component.js
@@ -1,17 +1,23 @@
 import React, {Component} from 'react';
-import { Link } from 'react-scroll';
+import { Link, Events, scrollSpy, animateScroll } from 'react-scroll';
+import styles from './Nav.stylesheet.css';
 
 class Nav extends Component {
+
+  componentDidMount() {
+    window.scrollTo(0, 2);
+  }
+
   render() {
     return (
-      <nav>
-        <Link to='Home' smooth={true} spy={true} duration={600}>Start</Link>
-        <Link to='WhiteWedding' smooth={true} spy={true} duration={600}>Ślub</Link>
-        <Link to='WeddingParty' smooth={true} spy={true} duration={600}>Wesele</Link>
-        <Link to='Hotel' smooth={true} spy={true} duration={600}>Hotel</Link>
-        <Link to='Transport' smooth={true} spy={true} duration={600}>Transport</Link>
-        <Link to='Gifts' smooth={true} spy={true} duration={600}>Prezenty</Link>
-        <Link to='Contact' smooth={true} spy={true} duration={600}>Kontakt</Link>
+      <nav className={styles.Nav}>
+        <Link to='Home' smooth={true} spy={true} duration={600} className={styles.link} activeClass={styles.currentLink}>Start</Link>
+        <Link to='WhiteWedding' smooth={true} spy={true} duration={600} className={styles.link} activeClass={styles.currentLink}>Ślub</Link>
+        <Link to='WeddingParty' smooth={true} spy={true} duration={600} className={styles.link} activeClass={styles.currentLink}>Wesele</Link>
+        <Link to='Hotel' smooth={true} spy={true} duration={600} className={styles.link} activeClass={styles.currentLink}>Hotel</Link>
+        <Link to='Transport' smooth={true} spy={true} duration={600} className={styles.link} activeClass={styles.currentLink}>Transport</Link>
+        <Link to='Gifts' smooth={true} spy={true} duration={600} className={styles.link} activeClass={styles.currentLink}>Prezenty</Link>
+        <Link to='Contact' smooth={true} spy={true} duration={600} className={styles.link} activeClass={styles.currentLink}>Kontakt</Link>
       </nav>
     )
   }
